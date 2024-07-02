452e3618901349b6a0447b7d5066347fe512f310
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-02 15:44:31 -0300

    Customize the admin form - corrigido

--- polls/admin.py
+++ polls/admin.py
@@ -5,11 +5,6 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInLine(admin.ModelAdmin):
-    model = Choice
-    extra = 3
-
-
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
